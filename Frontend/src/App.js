874ede26353773dd701c360edf1b8386c2b6096a
rohit <rohit2005pv@gmail.com>
2025-06-23 10:35:32 +0530

    Added frontend files with proper line endings

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // Import the CSS
+import Fileform from "./components/Fileform";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Leaf Disease Prediction</h1>
+      </header>
+      <Fileform />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/components/Fileform.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './Fileform.css';
+
+function Fileform() {
+    const [file, setFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+    const [prediction, setPrediction] = useState(null);
+
+    const HandleFileInputChange = (event) => {
+        const selectedFile = event.target.files[0];
+        setFile(selectedFile);
+
+        if (selectedFile) {
+            setPreview(URL.createObjectURL(selectedFile));
+        }
+    };
+
+    const HandleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const response = await fetch('http://127.0.0.1:8000/predict', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setPrediction(data);
+            } else {
+                console.error('Server error');
+            }
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
+    };
+
+    return (
+        <div className="fileform">
+            <form onSubmit={HandleSubmit}>
+                <input type="file" accept="image/*" onChange={HandleFileInputChange} />
+                <button type="submit">Predict</button>
+            </form>
+
+            {file && <p>Selected file: {file.name}</p>}
+
+            {preview && (
+                <div className="preview-container">
+                    <h3>Image Preview:</h3>
+                    <img src={preview} alt="Preview" className="preview-image"/>
+                </div>
+            )}
+
+            {prediction && (
+                <div className="prediction-result">
+                    <h2>Prediction Result:</h2>
+                    <p><strong>Class:</strong> {prediction.class}</p>
+                    <p><strong>Confidence:</strong> {prediction.confidence}%</p>
+
+                    
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Fileform;
