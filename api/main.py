577b97c65d61d6ba6bce6e4729d4955cea22e0ad
Rohitpinapatruni <147687761+Rohitpinapatruni@users.noreply.github.com>
2025-06-30 11:04:29 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -23,10 +23,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",  # for local testing
-        "https://leafdiseaseprediction25.netlify.app/",
-        "https://685fcb185e5b1684410b2b8a--leafdiseaseprediction25.netlify.app/",
-        "https://plantdiseaseprediction-backend.onrender.com"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
